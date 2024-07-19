82e53a72ce652bacd5d1050120b815155d4ed525
l0n3s0ld13r <kevin.g.valles@gmail.com>
2024-07-18 22:13:50 -0400

    Adding changes from module 3 part 2

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,15 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')  # Fields to display in the list view
+    search_fields = ('name', 'description')  # Fields to search in the admin interface
 
-# Register your models here.
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'type', 'year')  # Fields to display in the list view
+    search_fields = ('name', 'car_make__name')  # Fields to search in the admin interface
+    list_filter = ('type', 'year')  # Filters to add to the sidebar
 
-# CarModelInline class
-
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+# Register the models with the admin site
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
